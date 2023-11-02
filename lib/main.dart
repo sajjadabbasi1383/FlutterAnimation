dce49f23cc1787235c623abe32445e126f072686
sajjadabbasi1383 <sajjadabbas0083@gmail.com>
2023-11-02 15:44:19 +0330

    implement Home Screen and Animation catalog

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,6 @@
 import 'package:animations/home_screen.dart';
 import 'package:flutter/material.dart';
 
-import 'animated_container_boxdecoration_screen.dart';
-import 'animation_instagram_like.dart';
-import 'animation_rotate_screen.dart';
-import 'animation_scale_screen.dart';
-import 'hide_show_animated_screen.dart';
-import 'move_in_different_directions_screen.dart';
-
 void main() {
   runApp(const MyApp());
 }
